75646688ed8c743608d19d2c8642257ed46a9adc
bexcite <pavel.bashmakov@gmail.com>
2018-05-28 12:00:27 -0700

    test_slam with mansion world

--- hs_project/scripts/test_slam.sh
+++ hs_project/scripts/test_slam.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # TODO: From Testing SLAM
-x-terminal-emulator -e roslaunch turtlebot_gazebo turtlebot_world.launch &
+x-terminal-emulator -e roslaunch hs_project turtlebot_world_mansion.launch
+# x-terminal-emulator -e roslaunch turtlebot_gazebo turtlebot_world.launch gui:=false world_file:=../worlds/mansion_furnished.world &
 sleep 5 &&
 x-terminal-emulator -e roslaunch turtlebot_navigation gmapping_demo.launch &
 sleep 5 &&
