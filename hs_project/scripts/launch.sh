c3564ab0ba39ad83cfc8d30ab52cfd85ab8bf844
bexcite <pavel.bashmakov@gmail.com>
2018-05-28 10:34:17 -0700

    test_slam completed

--- hs_project/scripts/launch.sh
+++ hs_project/scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # TODO!
-xterm  -e  " gazebo " &
-sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" &
-sleep 5
-xterm  -e  " rosrun rviz rviz"
+# x-terminal-emulator  -e gazebo &
+# sleep 5
+x-terminal-emulator source /opt/ros/kinetic/setup.bash; roscore &
+# sleep 5
+# x-terminal-emulator  -e rosrun rviz rviz
--- hs_project/scripts/test_slam.sh
+++ hs_project/scripts/test_slam.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 # TODO: From Testing SLAM
+x-terminal-emulator -e roslaunch turtlebot_gazebo turtlebot_world.launch &
+sleep 5 &&
+x-terminal-emulator -e roslaunch turtlebot_navigation gmapping_demo.launch &
+sleep 5 &&
+x-terminal-emulator -e roslaunch turtlebot_rviz_launchers view_navigation.launch &
+sleep 5 &&
+x-terminal-emulator -e roslaunch turtlebot_teleop keyboard_teleop.launch &
