46e143a184749990120813ae6299314be8cfbcd5
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-05-31 09:19:24 -0700

    Add all in one [need to test]

---
+++ hs_project/scripts/home_service.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# All in one
+
+x-terminal-emulator -e roslaunch hs_project turtlebot_world_mansion.launch
+sleep 5
+
+x-terminal-emulator -e roslaunch hs_project amcl_demo_mansion.launch &
+sleep 5
+
+# x-terminal-emulator -e roslaunch turtlebot_rviz_launchers view_navigation.launch &
+x-terminal-emulator -e roslaunch hs_project view_navigation_marker.launch &
+sleep 5
+
+x-terminal-emulator -e rosrun add_markers add_markers &
+sleep 5
+
+x-terminal-emulator -e rosrun pick_objects pick_objects &
